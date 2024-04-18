317dc143a4844b82bffb1880ba1693b67ab15f69
WalterA <walteralb22@gmail.com>
2024-04-18 19:45:49 +0200

    finish 3-7

--- ProjectsVScode/Esercizi/Lezione2/lezione2.py
+++ ProjectsVScode/Esercizi/Lezione2/lezione2.py
@@ -71,8 +71,18 @@ print(f"{speak[1]} {transportation[0]}, {motorcycle[1]}")
 #• Start with your program from Exercise 3-4. Add a print() call at the end of your program, stating the name of the guest who can’t make it.
 #• Modify your list, replacing the name of the guest who can’t make it with the name of the new person you are inviting.
 #• Print a second set of invitation messages, one for each person who is still in your list.
-
-#list invit
+#3-6. More Guests: You just found a bigger dinner table, so now more space is available. Think of three more guests to invite to dinner.
+#• Start with your program from Exercise 3-4 or 3-5. Add a print() call to the end of your program, informing people that you found a bigger table.
+#• Use insert() to add one new guest to the beginning of your list.
+#• Use insert() to add one new guest to the middle of your list.
+#• Use append() to add one new guest to the end of your list.
+#• Print a new set of invitation messages, one for each person in your list.
+# 3-7. Shrinking Guest List: You just found out that your new dinner table won’t arrive in time for the dinner, and now you have space for only two guests.
+# • Start with your program from Exercise 3-6. Add a new line that prints a message saying that you can invite only two people for dinner.
+# • Use pop() to remove guests from your list one at a time until only two names remain in your list. Each time you pop a name from your list, print a message to that person letting them know you’re sorry you can’t invite them to dinner.
+# • Print a message to each of the two people still on your list, letting them know they’re still invited.
+# • Use del to remove the last two names from your list, so you have an empty list. Print your list to make sure you actually have an empty list at the end of your program.
+# #list invit
 invit: str=["pippo","bob","vieri","gianpippo2"]
 for i in invit:
     print("Invite", i)
@@ -83,4 +93,6 @@ print(invit)
 invit.insert(0, "gianfranco")
 invit.insert(3, "milimassimo")
 invit.append("gianfrippone")
+print(invit)
+del invit[0]
 print(invit)
