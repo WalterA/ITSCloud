1c0130a26f4ca3c148534a2a403f018401d13f97
WalterA <walteralb22@gmail.com>
2024-04-22 12:03:51 +0200

    sistemato

--- ProjectsVScode/Esercizi/Lezione2/22-04-2024.py
+++ ProjectsVScode/Esercizi/Lezione2/22-04-2024.py
@@ -34,20 +34,20 @@
 
 s: str = "La meccanica quantistica è la teoria fisica che descrive il comportamento della materia, della radiazione e le reciproche interazioni, con particolare riguardo ai fenomeni caratteristici della scala di lunghezza o di energia atomica e subatomica, dove le precedenti teorie classiche risultano inadeguate. Come caratteristica fondamentale, la meccanica quantistica descrive la radiazione e la materia sia come fenomeni ondulatori che come entità particellari, al contrario della meccanica classica, che descrive la luce solamente come un'onda e, ad esempio, l'elettrone solo come una particella. Questa inaspettata e controintuitiva proprietà della realtà fisica, chiamata dualismo onda-particella, è la principale ragione del fallimento delle teorie sviluppate fino al XIX secolo nella descrizione degli atomi e delle molecole. La relazione tra natura ondulatoria e corpuscolare è enunciata nel principio di complementarità e formalizzata nel principio di indeterminazione di Heisenberg. Esistono numerosi formalismi matematici equivalenti della teoria, come la meccanica ondulatoria e la meccanica delle matrici; al contrario, ne esistono numerose e discordanti interpretazioni riguardo all'essenza ultima del cosmo e della natura, che hanno dato vita a un dibattito tuttora aperto nell'ambito della filosofia della scienza. La meccanica quantistica rappresenta, assieme alla teoria della relatività, uno spartiacque rispetto alla fisica classica, portando alla nascita della fisica moderna. Attraverso la teoria quantistica dei campi, generalizzazione della formulazione originale che include il principio di relatività ristretta, essa è a fondamento di molte altre branche della fisica, come la fisica atomica, la fisica della materia condensata, la fisica nucleare, la fisica delle particelle, la chimica quantistica."
 #def counter(s:str) -> list[int, int, int, int]:
-    """
-        questa funzione prende una stringa in input e restituisce una lista costriita nel modo seguente:
-        -il primo elemento della lista contiene il numero di caratteri nella stringa
-        -il secondo elemento della lista contiene il numero di parole nella stringa
-        -il terzo elemento della lista contiene il numero di parole distinte nella stringa
-        -il quarto elemento della lista contene il numero di frasi nella stringa
-    """
-    lista=s.split(" ")
-    print("i parole sono",len(lista))
-    print("le caratteri sono",len(s))
-    paroledistinte=set(s.split())
-    print("le parole distinte sono",len(paroledistinte))
-    lista=s.split(".")
-    print("le frasi sono",len(lista))
+    # """
+    #     questa funzione prende una stringa in input e restituisce una lista costriita nel modo seguente:
+    #     -il primo elemento della lista contiene il numero di caratteri nella stringa
+    #     -il secondo elemento della lista contiene il numero di parole nella stringa
+    #     -il terzo elemento della lista contiene il numero di parole distinte nella stringa
+    #     -il quarto elemento della lista contene il numero di frasi nella stringa
+    # """
+    # lista=s.split(" ")
+    # print("i parole sono",len(lista))
+    # print("le caratteri sono",len(s))
+    # paroledistinte=set(s.split())
+    # print("le parole distinte sono",len(paroledistinte))
+    # lista=s.split(".")
+    # print("le frasi sono",len(lista))
 #fatta dal prof
 
 def counter(s:str) -> list[int]:
@@ -63,10 +63,4 @@ def counter(s:str) -> list[int]:
     #quanti frasi ha la stringa
     res.append=len(s.splint(".")) -1  #oppure res[-1]=s.count(".")
     return res
-
-
-
-
-
-
 counter(s)
